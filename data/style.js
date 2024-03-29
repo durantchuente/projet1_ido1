7d9fd49d01cba3a56a25e9e6872417f8075240f3
duran tchuente <duran.tchuente@gmail.com>
2024-03-29 12:58:14 -0400

    correction json struct

--- data/style.js
+++ data/style.js
@@ -1,12 +1,21 @@
-
-  $.get("/data", (data, status) => {
-    dataString = JSON.stringify(text);
-    dataJson = JSON.parse(dataString);
-    $('#temp2').html((dataJson.temperature.value).toFixed+ ` °C`)
-    $('#hum2').html((dataJson.humidity.value).toFixed+ ` %`)
-    $('#gas2').html((dataJson.gas.value).toFixed+ ` ppm`)
-  // $('#hum2').html(data.current.humidity+ ` %`)
-  // $('#alarmResponse').html(data.current.pressure_mb)
-  // $('#gas2').html(data.current.wind_kph+ ` ppm`)
-  });
-// });
+$.get("/data", (data, status) => {
+  dataJson = JSON.parse(data);
+  $('#temp2').html((dataJson.temperature.value).toFixed(2)+ ` °C`);
+  $('#hum2').html((dataJson.humidity.value).toFixed(2)+ ` %`);
+  $('#gas2').html((dataJson.gas.value).toFixed(2)+ ` ppm`);
+  $('#tempMessage').html(dataJson.temperature.message);
+  $('#humMessage').html(dataJson.humidity.message);
+  $('#co2Message').html(dataJson.gas.message);
+  if (dataJson.temperature.message == "Anormal")
+    $('#tempMessage').addClass( "text-danger" );
+  else
+    $('#tempMessage').addClass( "text-success" );
+  if (dataJson.humidity.message == "Anormal")
+    $('#humMessage').addClass( "text-danger" );
+  else
+    $('#humMessage').addClass( "text-success" );
+  if (dataJson.gas.message == "Anormal")
+    $('#co2Message').addClass( "text-danger" );
+  else
+    $('#co2Message').addClass( "text-success" );
+});
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+// import libraries
 #include <Arduino.h>
 #include <WiFi.h>
 #include "SPIFFS.h"
@@ -10,8 +11,9 @@
 #define DHTTYPE DHT22
 #define NOTE_B0  31
 
-const char* ssid = "UNIFI_IDO1";
-const char* password = "41Bidules!";
+//define variables
+const char* ssid = "daniel";
+const char* password = "allo1234";
 Sensor dht22Temp("Temperature", 27);
 Sensor dht22Hum("Humidite", 27);
 Sensor gas("Monoxyde de carbonne", 32);
@@ -25,7 +27,7 @@ StaticJsonDocument<100> jsonData;
 AsyncWebServer server(80);
 String getContentType(String path);
 
-
+//function for initialize SPIFFS
 void initSPIFFS() {
   if (!SPIFFS.begin(true)) {
     Serial.println("An error has occurred while mounting SPIFFS");
@@ -35,6 +37,7 @@ void initSPIFFS() {
   }
 }
 
+//function for initialize WIFI
 void initWiFi() {
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi ..");
@@ -52,35 +55,49 @@ void setup() {
   server.begin();
   dht.begin();
   pinMode(4, OUTPUT);
+  // collect data
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   int gasVal = analogRead(gas.getPin());
   dht22Temp.setData(t);
   dht22Hum.setData(h);
-  gas.setData(gasVal/100);
-
+  gas.setData(gasVal/10);
+  // implement routes server
   server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
+    //create json struct in string
     String buffer;
     jsonData["temperature"]["value"] = dht22Temp.getData();
-      jsonData["humdity"]["value"] = dht22Hum.getData();
-      jsonData["gas"]["value"] = gas.getData();
-      if (!gas.verificationNormalRange(400, 450))
-        jsonData["gas"]["message"] = "Anormal";
-      else
-        jsonData["gas"]["message"] = "Normal";
+    jsonData["humidity"]["value"] = dht22Hum.getData();
+    jsonData["gas"]["value"] = gas.getData();
+    if (!gas.verificationNormalRange(400, 450)) // verify condition range value
+      jsonData["gas"]["message"] = "Anormal";
+    else
+      jsonData["gas"]["message"] = "Normal";
 
-      if (!dht22Temp.verificationNormalRange(-25, 30))
-        jsonData["temperature"]["message"] = "Anormal";
-      else
-        jsonData["temperature"]["message"] = "Normal";
+    if (!dht22Temp.verificationNormalRange(-25, 30))
+      jsonData["temperature"]["message"] = "Anormal";
+    else
+      jsonData["temperature"]["message"] = "Normal";
 
-      if (!dht22Temp.verificationNormalRange(30, 80))
-        jsonData["humdity"]["message"] = "Anormal";
-      else
-        jsonData["humdity"]["message"] = "Normal";
-
-      serializeJson(jsonData, buffer);
-      request->send(200, "text/plain", buffer);
+    if (!dht22Hum.verificationNormalRange(30, 80))
+      jsonData["humidity"]["message"] = "Anormal";
+    else
+      jsonData["humidity"]["message"] = "Normal";
+    serializeJson(jsonData, buffer);
+    request->send(200, "text/plain", buffer);
+    if (!gas.verificationNormalRange(400, 450) || !dht22Temp.verificationNormalRange(-25, 30) || !dht22Temp.verificationNormalRange(30, 80))
+    {
+      digitalWrite(4, HIGH);
+      for (int thisNote = 0; thisNote < 1; thisNote  ) {
+        int noteDuration = 800/noteDurations[thisNote];
+        tone(33, melody[thisNote],noteDuration);
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+      }
+    }else{
+      noTone(33);
+      digitalWrite(4, LOW);
+    }
   });
   server.onNotFound([](AsyncWebServerRequest *request){
     String path = request->url();
@@ -89,25 +106,11 @@ void setup() {
     }
     request->send(SPIFFS, path);
   });
-
-  if (!gas.verificationNormalRange(400, 450) || !dht22Temp.verificationNormalRange(-25, 30) || !dht22Temp.verificationNormalRange(30, 80))
-  {
-    digitalWrite(4, HIGH);
-    for (int thisNote = 0; thisNote < 1; thisNote  ) {
-      int noteDuration = 800/noteDurations[thisNote];
-      tone(33, melody[thisNote],noteDuration);
-      int pauseBetweenNotes = noteDuration * 1.30;
-      delay(pauseBetweenNotes);
-      noTone(33);
-    }
-  }
 }
 
-void loop() {
+void loop() {}
 
-}
-
-String getContentType(String path) {
+String getContentType(String path) { // function of detect type file
   if (path.endsWith(".css")) {
     return "text/css";
   } else if (path.endsWith(".js")) {
